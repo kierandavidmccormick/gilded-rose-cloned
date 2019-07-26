fc567875945d9b6b2e9d32fcbd2e86f5cc0691e7
Kieran McCormick <kieranmccormick@Kierans-MBP.localdomain>
2019-07-26 14:42:05 -0500

    regex generalty expansion

--- src/RegexLibrary.java
+++ src/RegexLibrary.java
@@ -1,10 +1,10 @@
 public abstract class RegexLibrary {
-    public static final String SULFURAS = "(?i)(Conjured )?Sulfuras, Hand of Ragnaros";
-    public static final String AGEDBRIE = "(?i)(Conjured )?Aged Brie";
-    public static final String BACKSTAGEPASSES = "(?i)(Conjured )?Backstage Passes.*";
+    public static final String SULFURAS = "(?i)(Conjured(,)? )?Sulfuras(,)? Hand of Ragnaros.*";
+    public static final String AGEDBRIE = "(?i)(Conjured(,)? )?Aged(,)? Brie.*";
+    public static final String BACKSTAGEPASSES = "(?i)(Conjured(,)? )?Backstage(,)? Passes.*";
     public static final String CONJURED = "(?i)Conjured.*";
 
-    public static final String MAXVALSTRING = "(?i)(Int(eger)?)?(_|.|-| |:)?Max(imum)?(_|.|-| |:)?(Int(eger)?)?(_|.|-| |:)?(Value)?";
-    public static final String MINVALSTRING = "(?i)(Int(eger)?)?(_|.|-| |:)?Min(imum)?(_|.|-| |:)?(Int(eger)?)?(_|.|-| |:)?(Value)?";
+    public static final String MAXVALSTRING = "(?i)(Int(eger)?)?(_|.|-| |:)?Max(imum)?(_|.|-| |:)?(Int(eger)?)?(_|.|-| |:)?(Value)?.*";
+    public static final String MINVALSTRING = "(?i)(Int(eger)?)?(_|.|-| |:)?Min(imum)?(_|.|-| |:)?(Int(eger)?)?(_|.|-| |:)?(Value)?.*";
     public static final String ANYITEM = ".*";
 }
