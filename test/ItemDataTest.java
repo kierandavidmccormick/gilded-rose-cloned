a08273233ff2a360bf8996d751badc26146c9e49
Kieran McCormick <kieranmccormick@Kierans-MBP.localdomain>
2019-08-12 09:13:22 -0500

    finalized

--- test/ItemDataTest.java
+++ test/ItemDataTest.java
@@ -17,7 +17,6 @@ class ItemDataTest {
                         50, 0
                 )
         );
-        //TODO: deal with the longs that are supposed to be in the int... list
         ItemData itemData2 = new ItemData("Backstage Passes to a Concert", 10, 10, 50, 0, 4,
                 Integer.MIN_VALUE, 11, 1,
                 10, 6, 2,
